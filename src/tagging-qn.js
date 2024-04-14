c2d011b742dab9c6e104e31339cb83127ad8966e
Ivory Hodiny <hodiny.ivory@gmail.com>
2024-04-14 13:43:38 -0400

    made draggable and droppable

--- src/tagging-qn.js
+++ src/tagging-qn.js
@@ -10,6 +10,8 @@ export class TaggingQn extends DDD {
   constructor() {
     super();
     this.title = "Question Tags";
+    this.options=["AI generated","Beautiful","Good Form","Accessible"];
+    this.answers=[];
   }
 
   static get styles() {
@@ -18,7 +20,9 @@ export class TaggingQn extends DDD {
         css`
         :host {
         display: block;
-        
+      }
+      .tagging-wrapper{
+        padding:var(--ddd-spacing-2);
       }
       .answer-box,.options-box{
         display: inline-block;
@@ -29,24 +33,57 @@ export class TaggingQn extends DDD {
         border: solid 3px var(--ddd-theme-default-creekTeal);
         background:var(--ddd-theme-default-creekMaxLight);
       }
-      
+      .qn-image{
+        width:200px;
+        
+      }
+      .options-box{
+        display:flex;
+      }
+      .options{
+        border:solid 2px black;
+        padding:var(--ddd-spacing-2);
+        margin: var(--ddd-spacing-2);
+        width:150px;
+        height:30px; 
+      }
     
     `];
   }
 
+
   render() {
     return html`
-        <div >${this.title}</div>
+      <div class="tagging-wrapper"> 
+        <h1 >${this.title}</h1>
+        <img class="qn-image" src="images/haxpsu.png" alt="haxpsu">
         <div>${this.questiontext}</div>
-        <div class="answer-box">Drag Answers Here</div>
-        <div class="options-box">Options</div>
-        `;
+
+        <div class="answer-box" @drop="${(e) => this.drop(e, 'input-area')}" @dragover="${this.allowDrop}">Drag Answers Here</div>
+        ${this.answers.map((answer, index) => html`
+        <div class="answers-wrapper">
+                        <div class="answers" draggable="true" @dragstart="${() => this.drag(index)}">${answer}</div>
+                    </div>
+                    `)}
+                </div>
+
+        <div class="options-box">
+        ${this.options.map((question, index) => html`
+        <div class="options">
+          <div class="answers" draggable="true" @dragstart="${() => this.drag(index)}">${question}</div>
+        </div>
+        </div>
+        `)}
+      </div>
+    `;
   }
 
   static get properties() {
     return {
       title: { type: String },
       questiontext: {type:String},
+      options:{type:Array, reflect:true},
+      answers:{type:Array, reflect:true},
     };
   }
 }
